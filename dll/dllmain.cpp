6bbddfbdc60c504c1cc5e622949a4ed63fdb1748
Navin Pai <navinp@microsoft.com>
2019-06-11 17:54:59 -0700

    Add some comments and copyright code.

---
+++ dll/dllmain.cpp
@@ -0,0 +1,59 @@
+/*++
+
+    Copyright (c) Pai Financials LLC
+
+Module Name:
+
+    dllmain.cpp
+
+Abstract:
+
+    Defines the entry point for the DLL Application
+
+Author:
+
+    Navin Pai (navinp) - 06-Oct-2015
+
+--*/
+
+#include "pch.h"
+
+BOOL 
+APIENTRY 
+DllMain( 
+    HMODULE ModuleHandle,
+    DWORD  ReasonForCall,
+    LPVOID Reserved
+    )
+/*++
+
+Abstract:
+
+    The main entrypoint into the dll. This function reads the registry
+    settings and initializes the EarningsRelease class for use by
+    TradeStation
+
+--*/
+{
+    UNREFERENCED_PARAMETER(Reserved);
+
+    switch (ReasonForCall)
+    {
+    case DLL_PROCESS_ATTACH:
+        {
+            DisableThreadLibraryCalls(ModuleHandle);
+        }
+        break;
+
+    case DLL_PROCESS_DETACH:
+        {
+        }
+        break;
+
+    case DLL_THREAD_ATTACH:
+    case DLL_THREAD_DETACH:
+    }
+
+    return TRUE;
+}
+
